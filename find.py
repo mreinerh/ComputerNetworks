a06c3dbb3e911ba812fe8a4301e93bf382c88b2d
Alex Higgins <Alexanderplanetst@gmail.com>
2016-01-24 18:38:04 -0500

    Alex version of find.py

--- find.py
+++ find.py
@@ -1,50 +1,36 @@
-'''
-Created on Jan 16, 2016
+import sys
+import os
+import glob
 
-@author: reiner
-'''
-import os, sys, getopt
+#Check that only 3 command line arguments are given.
+if len(sys.argv) != 3:
+    print "Error in syntax."
+    print "Usage: find.py [start directory] [search phrase]"
+    print "Example: find.py . *.txt"
+    sys.exit(2)
 
-def find(directory, filename):{
-                               }
+#Get command line arguments
+startdir = sys.argv[1]
+keyword = sys.argv[2]
 
-"""this defines the parameters that the main method will take in and shows an
-    error message if the flags are not there. Executable is the first argument
-    followed by a flag of each argument
-"""
-def main(argv):
-    #initializes the directory name to null essentially
-    dirName = ''
-    #same with the file we want to search for
-    searchFile =''
-    #we 'try' to obtain the arguments from the command line
-    try:
-        opts, args = getopt.getopt(argv, "n:f:", ["dName=", "fName="])
-        #if not, we print out an error message
-    except getopt.GetoptError:
-        #usage message
-        print 'find.py -n [directory] -f [filename]'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'find.py [directory] -name [filename]'
-            sys.exit()
-        elif opt in ("-n", "dName"):
-            dirName = arg 
-        elif opt in ("-f", "fName"):
-            searchFile = arg
-    print 'Directory name is "', dirName
-    print 'File name is "', searchFile
-            
-  
+
+
+def rsearch(path):
+    """ Searches the given directory for files matching the keyword,
+        and recursively calls any subdirectories it finds."""
+
+
+    #Print out all files in this diectory matching the keyword syntax
+    for match in glob.glob(os.path.join(path,keyword)):
+        print match
+
+    #Gets all files in the dirctory 'path'
+    for file in os.listdir(path):
+        fullpath = os.path.join(path, file) #Gets the path relative to start dir
+        if(os.path.isdir(fullpath)): #If it is a directory, recursively call rsearch
+            rsearch(fullpath)
+
+
 if __name__ == "__main__":
-    print "~~~TESTING~~~"
-    main(sys.argv[1:])
-    print 'Number of arguments:', len(sys.argv), 'arguments.'
-    print 'Argument List:', str(sys.argv)
-    
-    rootDir = '.'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print('Found directory: %s' % dirName)
-    for fname in fileList:
-        print('\t%s' % fname)
+    """Starts the recursive search"""
+    rsearch(startdir)
