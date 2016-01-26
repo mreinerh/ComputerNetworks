f3cd02c1dcc9c63965e248906afd1e180d406a16
Alex Higgins <Alexanderplanetst@gmail.com>
2016-01-26 15:58:08 -0500

    Updated Comments for Dgram

--- echoDgramServer.py
+++ echoDgramServer.py
@@ -1,21 +1,22 @@
 import socket
 import sys
 
+#Network Info
 UDP_IP = "127.0.0.1"
 UDP_PORT = 7070
 
+#Create a new Dgram socket
 sock = socket.socket(socket.AF_INET,#Internet
                        socket.SOCK_DGRAM)#Specify Datagram
-
+#Try to bind it to a port
 try:
-
     sock.bind((UDP_IP,UDP_PORT))
     print "Socket bound to: " + UDP_IP + ":" + str(UDP_PORT)
-except Exception as e:
+except Exception as e: #If there is an error, print the error
     print e
     sys.exit(1)
 
-
+#Listen until a connection is made. Print out the data and address, then exit.
 while True:
     data, addr = sock.recvfrom(2048)#Buffer size
     print "Data: " + data
--- helloDgramClient.py
+++ helloDgramClient.py
@@ -1,18 +1,20 @@
 import socket
 
+#Network Info
 UDP_IP = "127.0.0.1"
 UDP_PORT = 7070
 
-
+#Create a new Dgram socket
 sock = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
 
+#Try to send a message over the socket.
 try:
     message = "Hello World"
     sock.sendto(message, (UDP_IP, UDP_PORT))
     print "Sent: " + message
-except Exception as e:
+except Exception as e: #Print an error, if there is any
     print e.message
-finally:
+finally: #Close the socket
     print "Goodbye"
     sock.close()
